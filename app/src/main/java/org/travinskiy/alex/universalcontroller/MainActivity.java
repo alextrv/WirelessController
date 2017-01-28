8bbc415a0ba306ec3a1a270362be95772509b0cc
Alexander Travinskiy <alex.trv92@gmail.com>
2017-01-28 14:39:39 +0200

    Initial commit

---
+++ app/src/main/java/org/travinskiy/alex/universalcontroller/MainActivity.java
@@ -0,0 +1,13 @@
+package org.travinskiy.alex.universalcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
