d767a61739f4f7a62ce46af4417065c07e8930c5
Alexander Travinskiy <alex.trv92@gmail.com>
2017-01-28 23:52:37 +0200

    Added preference WifiWhitelist

--- app/src/main/java/org/travinskiy/alex/universalcontroller/AppPreferences.java
+++ app/src/main/java/org/travinskiy/alex/universalcontroller/AppPreferences.java
@@ -3,9 +3,12 @@ package org.travinskiy.alex.universalcontroller;
 import android.content.Context;
 import android.preference.PreferenceManager;
 
+import java.util.Set;
+
 public class AppPreferences {
 
     private static final String PREF_STOP_IN_AIRPLANE_MODE = "prefStopInAirplaneMode";
+    private static final String PREF_WIFI_WHITELIST = "prefWifiWhitelist";
 
     public static boolean getPrefStopInAirplaneMode(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
@@ -16,6 +19,18 @@ public class AppPreferences {
         PreferenceManager.getDefaultSharedPreferences(context)
                 .edit()
                 .putBoolean(PREF_STOP_IN_AIRPLANE_MODE, value)
+                .apply();
+    }
+
+    public static Set<String> getPrefWifiWhitelist(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getStringSet(PREF_WIFI_WHITELIST, null);
+    }
+
+    public static void setPrefWifiWhitelist(Context context, Set<String> values) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putStringSet(PREF_WIFI_WHITELIST, values)
                 .apply();
     }
 
