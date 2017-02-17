11f0f91a992dcba440f08dd2108452d51077fcf8
Alexander Travinskiy <alex.trv92@gmail.com>
2017-02-17 13:02:14 +0200

    Added new prefs

--- app/src/main/java/org/travinskiy/alex/universalcontroller/AppPreferences.java
+++ app/src/main/java/org/travinskiy/alex/universalcontroller/AppPreferences.java
@@ -16,6 +16,9 @@ public class AppPreferences {
     public static final String PREF_END_TIME = "prefEndTime";
     public static final String PREF_REPEAT_DISABLE_WIRELESS = "prefRepeatDisableWireless";
     public static final String PREF_SERVICE_RUN_INTERVAL = "prefServiceRunInterval";
+    public static final String PREF_FORCE_DISABLED_WIRELESS = "prefForceDisabledWireless";
+    public static final String PREF_DISABLE_WIRELESS = "prefDisableWireless";
+    public static final String PREF_DISABLE_BLUETOOTH = "prefDisableBluetooth";
 
     public static boolean getPrefStopInAirplaneMode(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
@@ -77,6 +80,18 @@ public class AppPreferences {
                 .apply();
     }
 
+    public static String getPrefEndTime(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_END_TIME, TimePickerFragment.START_TIME_DEFAULT);
+    }
+
+    public static void setPrefEndTime(Context context, String value) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_END_TIME, value)
+                .apply();
+    }
+
     public static int getPrefRepeatDisableWireless(Context context) {
         String value = PreferenceManager.getDefaultSharedPreferences(context)
                 .getString(PREF_REPEAT_DISABLE_WIRELESS, context.getString(R.string.pref_repeatDisableWireless_default));
@@ -94,6 +109,42 @@ public class AppPreferences {
         String value = PreferenceManager.getDefaultSharedPreferences(context)
                 .getString(PREF_SERVICE_RUN_INTERVAL, context.getString(R.string.pref_serviceRunInterval_default));
         return Integer.parseInt(value);
+    }
+
+    public static boolean getPrefForceDisabledWireless(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(PREF_FORCE_DISABLED_WIRELESS, false);
+    }
+
+    public static void setPrefForceDisabledWireless(Context context, boolean value) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putBoolean(PREF_FORCE_DISABLED_WIRELESS, value)
+                .apply();
+    }
+
+    public static boolean getPrefDisableWireless(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(PREF_DISABLE_WIRELESS, false);
+    }
+
+    public static void setPrefDisableWireless(Context context, boolean value) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putBoolean(PREF_DISABLE_WIRELESS, value)
+                .apply();
+    }
+
+    public static boolean getPrefDisableBluetooth(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(PREF_DISABLE_BLUETOOTH, true);
+    }
+
+    public static void setPrefDisableBluetooth(Context context, boolean value) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putBoolean(PREF_DISABLE_BLUETOOTH, value)
+                .apply();
     }
 
 }
